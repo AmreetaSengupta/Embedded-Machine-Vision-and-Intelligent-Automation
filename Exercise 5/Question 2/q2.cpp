6f4eecc9cf85e946a19554dc2b44f073250cbc63
amreeta <amreeta.11@gmail.com>
2019-07-26 22:51:02 -0600

    Updates

--- Exercise 5/Question 2/q2.cpp
+++ Exercise 5/Question 2/q2.cpp
@@ -5,6 +5,7 @@
 * Date: 07/26/2019
 * References: docs.opencv.org
 *             https://answers.opencv.org/question/179816/human-detection-and-tracking-opencv-c/
+* 			  Embedded Machine Vision and Intelligent Automation Exercise 2 by Amreeta Sengupta
 ***********************************************************************************************************/
 
 #include <unistd.h>
@@ -16,17 +17,23 @@
 #include <opencv2/opencv.hpp>
 
 #define ffmpeg_command "ffmpeg -r 30 -f Image2 -i Frame_%d.jpeg "
+#define ns 1000000000
 
 using namespace cv;
 using namespace std;
 
+struct timespec start,stop;
+
 int main( int argc, char** argv )
 {
-	  int i,frame_num = 0;
+	  int i,frame_num = 0, frame_count = 0;
 	  int show_flag=0, log_flag=0, store_flag=0;
+	  int count_val = 0;
 	  char buff[100];
 	  string str1;
 	  const string src = argv[1];
+	  double timestamp, avg_time;
+      double max_val=0;
 	  Mat present_frame, result_frame;
 	  openlog("LOG_RESULT",LOG_CONS,LOG_USER);
 	  
@@ -59,7 +66,8 @@ int main( int argc, char** argv )
     
 	  while(1)
 	  {
-		  
+		
+		clock_gettime( CLOCK_REALTIME, &start); 
 	    cap >> present_frame;
 	  
 	    // If the frame is empty, break immediately
@@ -80,6 +88,17 @@ int main( int argc, char** argv )
             Rect r = found[i];
             rectangle(result_frame, found[i], cv::Scalar(0,0,255), 3);
         }
+        
+        clock_gettime( CLOCK_REALTIME, &stop);
+        
+        // Timestamp Calculation
+        timestamp = (double)(stop.tv_sec - start.tv_sec + (double) (stop.tv_nsec - start.tv_nsec)/(double) ns); 
+        avg_time += timestamp;
+		if(timestamp > max_val)
+		{
+			max_val = timestamp; // Calculating the maximum execution time of frames for analysis
+		}
+		count_val++; 
 
         // Display the result
         if(show_flag == 1)
@@ -97,20 +116,26 @@ int main( int argc, char** argv )
 		// Log the result
 		 if(log_flag == 1)
 		 {
-			syslog(LOG_INFO, "Number of Pedestrians detected = %d ", found.size());
+			syslog(LOG_INFO, "Number of Pedestrians detected in Frame %d = %d ", frame_count++,(int)found.size());
 			for( size_t i = 0; i < found.size(); i++ )
 			{
-				syslog(LOG_INFO, "Coordinates of Pedastrian %d: x=%d y=%d\n",i,found[i].x+found[i].width/2,found[i].y+found[i].height/2);
+				syslog(LOG_INFO, "Coordinates of Pedastrian %d: x=%d y=%d\n",(int)i,(int)(found[i].x+found[i].width/2),(int)(found[i].y+found[i].height/2));
 			}
 		}
+		
 
 	    // Press  ESC on keyboard to exit
 	    char c = cvWaitKey(33);
         if( c == 27 ) break;
         
 	  }
-	     
-	  system(str1.c_str());
+	  
+	  // Execution Time analysis
+	  printf("Average Time = %lf seconds\n",avg_time/count_val);
+	  printf("Max Time = %lf seconds\n",max_val);
+	  
+	  // Re-encoding the video
+	  system(str1.c_str()); 
 	  
 	  // When everything done, release the video capture object
 	  cap.release();
